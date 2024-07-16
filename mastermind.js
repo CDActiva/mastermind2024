78c1244e7cede27686de369e3d7cffbf91ba5c20
janosoler <janosolerdiaz@gmail.com>
2024-07-16 13:32:53 +0200

    gestión click sobre botones de combinación actual

--- mastermind.js
+++ mastermind.js
@@ -1,5 +1,13 @@
 const MAX_COMBINATION_LENGTH = 4;
 
+function createCurrentCombination(){
+    let currentCombination = [];
+    for(let i=0; i<=MAX_COMBINATION_LENGTH-1; i++){
+        currentCombination.push("bg-gray");
+    }
+    return currentCombination;
+}
+
 function paintCurrentCombination(currentCombination){
     const currentCombinationButtons = document.querySelectorAll(".current_combination_button");
     for(let i=0; i<=currentCombination.length-1; i++){
@@ -10,13 +18,25 @@ function paintCurrentCombination(currentCombination){
 
 
 const colorButtons = document.querySelectorAll(".color_option_button");
-let currentCombination = [];
+const currentCombination = createCurrentCombination();
 for(let i=0; i<=colorButtons.length-1; i++){
-    colorButtons[i].addEventListener("click", (event)=>{
-        if (currentCombination.length<MAX_COMBINATION_LENGTH) {
-            currentCombination.push(event.target.classList[1]);
-            paintCurrentCombination(currentCombination);
+    colorButtons[i].addEventListener("click", (event)=>{  
+        for(let j=0; j<=currentCombination.length-1; j++){
+            if (currentCombination[j]=="bg-gray"){
+                currentCombination[j]=event.target.classList[1];
+                paintCurrentCombination(currentCombination);
+                break;
+            }
         }
-        
+        //currentCombination.push(event.target.classList[1]); 
+    })
+}
+
+const currentCombinationButtons = document.querySelectorAll(".current_combination_button");
+for(let i=0; i<=currentCombinationButtons.length-1; i++){
+    currentCombinationButtons[i].addEventListener("click", (event)=>{
+        event.target.classList.remove(currentCombination[i]);
+        event.target.classList.add("bg-gray");
+        currentCombination[i]="bg-gray";
     })
 }
