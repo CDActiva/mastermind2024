6e7171af46ae5ad426641dba437aaa73a790434d
janosoler <janosolerdiaz@gmail.com>
2024-07-22 12:04:30 +0200

    añadir combonación al historico

--- mastermind.js
+++ mastermind.js
@@ -1,11 +1,18 @@
 const MAX_COMBINATION_LENGTH = 4;
 
 function createCurrentCombination(){
-    let currentCombination = [];
+    let newCurrentCombination = [];
     for(let i=0; i<=MAX_COMBINATION_LENGTH-1; i++){
-        currentCombination.push("bg-gray");
+        newCurrentCombination.push("bg-gray");
     }
-    return currentCombination;
+    return newCurrentCombination;
+}
+
+function resetCurrentCombinationColors(currentCombination){
+    const currentCombinationButtons = document.querySelectorAll(".current_combination_button");
+    for(let i=0; i<=currentCombination.length-1; i++){
+        currentCombinationButtons[i].setAttribute("class", "current_combination_button bg-gray");
+    }
 }
 
 function paintCurrentCombination(currentCombination){
@@ -16,9 +23,40 @@ function paintCurrentCombination(currentCombination){
     }
 }
 
+function validateCurrentCombination(combination){
+    let isValidCombination = false;
+    let totalOfGreyElementsInCombination = 0;
+    for (let i=0; i<=combination.length-1; i++){
+        if (combination[i]=="bg-gray") totalOfGreyElementsInCombination++;
+    }
+    if (totalOfGreyElementsInCombination==0) isValidCombination=true;
+    return isValidCombination;
+}
+
+//comprobar que no haya ningún gris
+//resetear Current Combination (vuelva a ser todos grises)
+function addCurrentCombinationToHistoric(){
+    if (validateCurrentCombination(currentCombination)){
+        let newHistoricCombination = document.createElement("div");
+        newHistoricCombination.classList.add("historic_combination");
+        for(let i=0; i<=currentCombination.length-1; i++){
+            let newSquareColor = document.createElement("div");
+            newSquareColor.classList.add("historic_square");
+            newSquareColor.classList.add(currentCombination[i]);
+            newHistoricCombination.insertAdjacentElement("beforeend", newSquareColor);
+        }
+        document.querySelector("#historial").insertAdjacentElement("afterbegin", newHistoricCombination);
+        currentCombination = createCurrentCombination();
+        resetCurrentCombinationColors(currentCombination);
+    } else {
+        window.alert("Tu combinación de colores no es válida. Tienes que elegir 4 colores");
+    }
+    
+}
+
 
 const colorButtons = document.querySelectorAll(".color_option_button");
-const currentCombination = createCurrentCombination();
+let currentCombination = createCurrentCombination();
 for(let i=0; i<=colorButtons.length-1; i++){
     colorButtons[i].addEventListener("click", (event)=>{  
         for(let j=0; j<=currentCombination.length-1; j++){
@@ -40,3 +78,5 @@ for(let i=0; i<=currentCombinationButtons.length-1; i++){
         currentCombination[i]="bg-gray";
     })
 }
+
+document.querySelector("#confirm_current_combination").addEventListener("click", addCurrentCombinationToHistoric);
