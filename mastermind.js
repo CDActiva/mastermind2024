4bcc48f97647c2d700e06b4e2485adbd4adc3a82
janosoler <janosolerdiaz@gmail.com>
2024-07-23 12:00:10 +0200

    combinación objetivo y condiciones de victoria y derrota

--- mastermind.js
+++ mastermind.js
@@ -1,4 +1,6 @@
 const MAX_COMBINATION_LENGTH = 4;
+const COLOR_OPTIONS = ["bg-coral", "bg-yellow", "bg-purple", "bg-black", "bg-green", "bg-blue"];
+const MAX_ATTEMPTS = 10;
 
 function createCurrentCombination(){
     let newCurrentCombination = [];
@@ -7,6 +9,18 @@ function createCurrentCombination(){
     }
     return newCurrentCombination;
 }
+
+function createTargetCombination(combinationLength, colorOptions){
+    let targetCombination = [];
+    let randomIndex; 
+    for(let i=0; i<=combinationLength-1; i++){
+        randomIndex = Math.floor(Math.random()*colorOptions.length);
+        targetCombination.push(colorOptions[randomIndex]);
+    }
+    return targetCombination;
+}
+
+
 
 function resetCurrentCombinationColors(currentCombination){
     const currentCombinationButtons = document.querySelectorAll(".current_combination_button");
@@ -33,10 +47,22 @@ function validateCurrentCombination(combination){
     return isValidCombination;
 }
 
+function combinationsAreEqual(currentCombination, targetCombination){
+    let areCombinationsEqual = true;
+    for(let i=0; i<=currentCombination.length-1; i++){
+        if (currentCombination[i]!=targetCombination[i]){
+            areCombinationsEqual = false;
+            break;
+        }
+    }
+    return areCombinationsEqual;
+}
+
 //comprobar que no haya ningún gris
 //resetear Current Combination (vuelva a ser todos grises)
 function addCurrentCombinationToHistoric(){
     if (validateCurrentCombination(currentCombination)){
+        console.log(currentCombination);
         let newHistoricCombination = document.createElement("div");
         newHistoricCombination.classList.add("historic_combination");
         for(let i=0; i<=currentCombination.length-1; i++){
@@ -46,8 +72,14 @@ function addCurrentCombinationToHistoric(){
             newHistoricCombination.insertAdjacentElement("beforeend", newSquareColor);
         }
         document.querySelector("#historial").insertAdjacentElement("afterbegin", newHistoricCombination);
+        numberOfAttempts++;
+        const isUserWinner = combinationsAreEqual(currentCombination, targetCombination);
+        if (isUserWinner) window.alert("Has ganado, la última combinación era la correcta");
+        const isGameOver = numberOfAttempts == MAX_ATTEMPTS;
+        if (isGameOver && !isUserWinner) window.alert(`Game Over. La combinación correcta es ${targetCombination}`);
         currentCombination = createCurrentCombination();
         resetCurrentCombinationColors(currentCombination);
+        
     } else {
         window.alert("Tu combinación de colores no es válida. Tienes que elegir 4 colores");
     }
@@ -56,6 +88,9 @@ function addCurrentCombinationToHistoric(){
 
 
 const colorButtons = document.querySelectorAll(".color_option_button");
+const targetCombination = createTargetCombination(MAX_COMBINATION_LENGTH, COLOR_OPTIONS);
+console.log(targetCombination);
+let numberOfAttempts = 0;
 let currentCombination = createCurrentCombination();
 for(let i=0; i<=colorButtons.length-1; i++){
     colorButtons[i].addEventListener("click", (event)=>{  
